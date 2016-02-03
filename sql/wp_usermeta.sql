ad87d9b9717253b138f7e591c2abd0194765598e
Sam Urquhart <sam@mrstth.com>
2016-02-03 12:27:16 +0000

    db snapshot

---
+++ sql/wp_usermeta.sql
@@ -0,0 +1,69 @@
+-- MySQL dump 10.15  Distrib 10.0.23-MariaDB, for debian-linux-gnu (x86_64)
+--
+-- Host: localhost    Database: gft
+-- ------------------------------------------------------
+-- Server version	10.0.23-MariaDB-1~wheezy-log
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8 */;
+/*!40103 SET @OLD_TIME_ZONE=@@TIME_ZONE */;
+/*!40103 SET TIME_ZONE='+00:00' */;
+/*!40014 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0 */;
+/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
+/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
+/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
+
+--
+-- Table structure for table `wp_usermeta`
+--
+
+DROP TABLE IF EXISTS `wp_usermeta`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `wp_usermeta` (
+  `umeta_id` bigint(20) unsigned NOT NULL AUTO_INCREMENT,
+  `user_id` bigint(20) unsigned NOT NULL DEFAULT '0',
+  `meta_key` varchar(255) COLLATE utf8mb4_unicode_ci DEFAULT NULL,
+  `meta_value` longtext COLLATE utf8mb4_unicode_ci,
+  PRIMARY KEY (`umeta_id`),
+  KEY `user_id` (`user_id`),
+  KEY `meta_key` (`meta_key`(191))
+) ENGINE=InnoDB AUTO_INCREMENT=16 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `wp_usermeta`
+--
+
+LOCK TABLES `wp_usermeta` WRITE;
+/*!40000 ALTER TABLE `wp_usermeta` DISABLE KEYS */;
+INSERT INTO `wp_usermeta` VALUES (1,1,'nickname','g-flAdm1n');
+INSERT INTO `wp_usermeta` VALUES (2,1,'first_name','');
+INSERT INTO `wp_usermeta` VALUES (3,1,'last_name','');
+INSERT INTO `wp_usermeta` VALUES (4,1,'description','');
+INSERT INTO `wp_usermeta` VALUES (5,1,'rich_editing','true');
+INSERT INTO `wp_usermeta` VALUES (6,1,'comment_shortcuts','false');
+INSERT INTO `wp_usermeta` VALUES (7,1,'admin_color','fresh');
+INSERT INTO `wp_usermeta` VALUES (8,1,'use_ssl','0');
+INSERT INTO `wp_usermeta` VALUES (9,1,'show_admin_bar_front','true');
+INSERT INTO `wp_usermeta` VALUES (10,1,'wp_capabilities','a:1:{s:13:\"administrator\";b:1;}');
+INSERT INTO `wp_usermeta` VALUES (11,1,'wp_user_level','10');
+INSERT INTO `wp_usermeta` VALUES (12,1,'dismissed_wp_pointers','');
+INSERT INTO `wp_usermeta` VALUES (13,1,'show_welcome_panel','1');
+INSERT INTO `wp_usermeta` VALUES (14,1,'session_tokens','a:1:{s:64:\"19c82ebd047731f1032fb3f637ccf23d38c660f464d78c37029909c1f63f969e\";a:4:{s:10:\"expiration\";i:1454674815;s:2:\"ip\";s:12:\"2.30.193.189\";s:2:\"ua\";s:65:\"Mozilla/5.0 (Windows NT 6.1; rv:43.0) Gecko/20100101 Firefox/43.0\";s:5:\"login\";i:1454502015;}}');
+INSERT INTO `wp_usermeta` VALUES (15,1,'wp_dashboard_quick_press_last_post_id','3');
+/*!40000 ALTER TABLE `wp_usermeta` ENABLE KEYS */;
+UNLOCK TABLES;
+/*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
+
+/*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
+/*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
+/*!40014 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS */;
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
+/*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
+
+-- Dump completed
